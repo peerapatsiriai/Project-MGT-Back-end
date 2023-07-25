7170031e7382e34fbef61817603b59d6d0ca820c
peerapatsiriai <peecorei1@gmial.com>
2023-07-25 09:13:56 +0700

    change new databasue 1

--- dbconfig.js
+++ dbconfig.js
@@ -7,7 +7,7 @@ var dbconfig = {
         password: '',
         database: 'project_mgt'
     },
-    production: {
+    production1: {
         //connectionLimit : 10,
         //host: '128.199.188.223',
         host: 'yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
@@ -15,6 +15,15 @@ var dbconfig = {
         user: 'm1i0apdnwx6uuqhx',
         password: 'm56uhwdpjpsoa44v',
         database: 'cff5144ks6iiu375'
-    }
+    },
+    production2: {
+        //connectionLimit : 10,
+        //host: '128.199.188.223',
+        host: 'wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        port: '3306',
+        user: 'uotdx2ldu03w47k5',
+        password: 'ujt4mrw25n2ajeja',
+        database: 'duidokltr1gf7r1g'
+    },
 };
 module.exports = dbconfig;
--- env.js
+++ env.js
@@ -1,3 +1,4 @@
 // var env = process.env.NODE_ENV || 'development';
-var env = process.env.NODE_ENV || 'production'; 
+// var env = process.env.NODE_ENV || 'production1'; 
+var env = process.env.NODE_ENV || 'production2'; 
 module.exports = env;
