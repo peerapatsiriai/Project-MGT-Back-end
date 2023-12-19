bfc90e95d0afb77b5aef1517f648aa1bee3c9134
peerapatsiriai <peecorei1@gmial.com>
2023-12-20 01:46:36 +0700

    change to online dev

--- dbconfig.js
+++ dbconfig.js
@@ -9,10 +9,10 @@ var dbconfig = {
         //database: 'ce_project_mgt'
     },
     Dev_online: {
-        host: "sql6.freesqldatabase.com",
-        user: "sql6635663",
-        password: "NktAhYk6bD",
-        database: "sql6635663",
+        host: "grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "x30lzoxzakd7mmfb",
+        password: "ssd7zoad8t4r9e20",
+        database: "us9k7p5quhhayigx",
         port: "3306"
     },
     Production: {
--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-var env = process.env.NODE_ENV || 'Dev_local';
-// var env = process.env.NODE_ENV || 'Dev_online'; 
+//var env = process.env.NODE_ENV || 'Dev_local';
+var env = process.env.NODE_ENV || 'Dev_online'; 
 // // var env = process.env.NODE_ENV || 'production2'; 
 module.exports = env;
